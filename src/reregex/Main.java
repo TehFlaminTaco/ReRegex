fb029a00f045c316f19ca4d1631e70e518b16bd6
PavelBraginskiy <pavelbraginskiy@hotmail.com>
2017-03-23 23:11:23 -0700

    Fixed

--- src/reregex/Main.java
+++ src/reregex/Main.java
@@ -18,17 +18,16 @@ public class Main {
 		String[] code = read_code(target).replaceAll("(?<!\\\\)#[^\r\n]*", "")
 										 .replaceAll("\r?\n", "")
 										 .replaceAll("\\(\\?#.*?\\)", "")
-										 .split("(?<!\\\\)/");
+		String toexecute;								 .split("(?<!\\\\)/");
 		if(code.length%2!=1){
-			System.err.println("No default code detected, nothing to run.");
-			return;
+			toexecute=new Scanner(System.in).useDelimiter("\\Z").next();
+		} else {
+			toexecute = code[code.length-1];
 		}
 		for(int i=0; i<code.length-1;i+=2){
 			regexes.put(code[i], code[i+1].replaceAll("\\\\/", "/"));
 		}
-		
-		String toexecute = code[code.length-1];
-		String old=new Scanner(System.in).useDelimiter("\\Z").next();
+		String old="";
 		while(!toexecute.equals(old)){
 			old = toexecute;
 			for(String e : regexes.keySet()){
